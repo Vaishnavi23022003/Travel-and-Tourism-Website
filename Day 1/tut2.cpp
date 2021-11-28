dce64d61b6d4ef58b934a7c758084cdcd353927f
Vaishnavi23022003 <vaishnavibadony@gmail.com>
2021-11-28 18:17:01 +0530

    original commit

---
+++ Day 1/tut2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+	
+	int a = 34;
+	cout << "the value of a was" << a << endl;
+	int a = 45;
+	cout << "the value of a is" << a << endl;
+	return 0;
+}
